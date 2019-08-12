27ad62db1e932d93e1cd1f04935b66939b1d07bf
karthik rm <rmkarthik@hotmail.com>
2019-08-11 22:33:24 -0400

    Added logic and styling

--- script/player.js
+++ script/player.js
@@ -7,15 +7,12 @@ function Player(classType, health, manner, strength, agility, speed){
     this.strength = strength;
     this.agility = agility;
     this.speed = speed;
-};
+}
 
 let PlayerMoves = {
     calcAttack: function (){
-        let getPlayerSpeed = player.speed;
-        let getEnemySpeed = enemy.speed;
-    },
-
-    playerAttack : function() {
+        
+      let playerAttack = function() {
         let calcBaseDamage;
         if(player.manner >0 ){
             calcBaseDamage = player.strength * player.manner / 1000;
@@ -26,14 +23,15 @@ let PlayerMoves = {
         let offsetDamage = Math.floor(Math.random() * Math.floor(10));
 
         let calcOutputDamage = calcBaseDamage + offsetDamage;
-        let numberOfHits = Math.floor(Math.random * Math.floor(player.agility/2)+1);
-
-        let attackValues = [calcOutputDamage , numberOfHits];
+        let numberOfHits = Math.floor(Math.random() * Math.floor(20));
+        console.log(numberOfHits);
+        console.log(calcOutputDamage);
+        let attackValues = [calcOutputDamage,numberOfHits];
 
         return attackValues;
-    },
-
-     enemyAttack : function() {
+    }
+    //enemy attacks
+     let enemyAttack = function() {
         let calcBaseDamage;
         if(enemy.manner >0 ){
             calcBaseDamage = enemy.strength * enemy.manner / 1000;
@@ -44,10 +42,79 @@ let PlayerMoves = {
         let offsetDamage = Math.floor(Math.random() * Math.floor(10));
 
         let calcOutputDamage = calcBaseDamage + offsetDamage;
-        let numberOfHits = Math.floor(Math.random * Math.floor(enemy.agility/2)+1);
-
-        let attackValues = [calcOutputDamage , numberOfHits];
+        let numberOfHits = Math.floor(Math.random() * Math.floor(20));
+        console.log(numberOfHits);
+        console.log(calcOutputDamage);
+        let attackValues = [calcOutputDamage,numberOfHits];
 
         return attackValues;
     }
+    
+    let getPlayerSpeed = player.speed;
+    let getEnemySpeed = enemy.speed;
+    let playerHealth = document.querySelector(".health-player");
+    let enemyHealth = document.querySelector(".health-enemy");
+    let status = document.querySelector(".status");
+    if (getPlayerSpeed >= getEnemySpeed){
+            let playerAttackValues = playerAttack();
+            let totalDamage = playerAttackValues[0]*playerAttackValues[1];
+
+            enemy.health = enemy.health - totalDamage;
+            
+            status.innerHTML = '<p>You have hit ' + playerAttackValues[0] + ' damage ' +playerAttackValues[1]+ ' times</p>';
+
+            if(enemy.health <= 0){
+                status.innerHTML = "<p>You win!! Refresh to play again</p>";
+                playerHealth.innerHTML = 'Health: ' +player.health;
+                enemyHealth.innerHTML = 'Health: 0';
+            } else {
+                enemyHealth.innerHTML = 'Health: ' +enemy.health;
+
+                let enemyAttackValues = enemyAttack();
+                let totalDamage = enemyAttackValues[0]*enemyAttackValues[1];
+
+                player.health = player.health - totalDamage;
+                
+                status.innerHTML = "<p>Enemy have hit " + enemyAttackValues[0] + " damage " +enemyAttackValues[1]+ " times</p>";
+    
+                if(player.health <= 0){
+                    status.innerHTML = "<p>You lose!! Refresh to play again</p>";
+                    playerHealth.innerHTML = 'Health: 0';
+                    enemyHealth.innerHTML = 'Health: ' +enemy.health;
+            } else {
+                    playerHealth.innerHTML = 'Health: ' +player.health;
+            }
+        }
+    } else {
+            let enemyAttackValues = enemyAttack();
+            let totalDamage = enemyAttackValues[0]*enemyAttackValues[1];
+
+            player.health = player.health - totalDamage;
+            
+            status.innerHTML = "<p>Enemy have hit " + enemyAttackValues[0] + " damage " +enemyAttackValues[1]+ " times</p>";
+
+            if(player.health <= 0){
+                status.innerHTML = "<p>You lose!! Refresh to play again</p>";
+                enemyHealth.innerHTML = 'Health: ' +enemy.health;
+                playerHealth.innerHTML = 'Health: 0';
+            } else {
+                playerHealth.innerHTML = 'Health: ' +player.health;
+
+                let playerAttackValues = playerAttack();
+                let totalDamage = playerAttackValues[0]*playerAttackValues[1];
+
+                enemy.health = enemy.health - totalDamage;
+                
+                status.innerHTML = "<p>You have hit " + playerAttackValues[0] + " damage " +playerAttackValues[1]+ " times</p>";
+    
+                if(enemy.health <= 0){
+                    status.innerHTML = "<p>You Won!! Refresh to play again</p>";
+                    enemyHealth.innerHTML = 'Health: 0';
+                    playerHealth.innerHTML = 'Health: ' +player.health;
+            } else {
+                    enemyHealth.innerHTML = 'Health: ' +enemy.health;
+            }
+        }
+    }
+}
 }
